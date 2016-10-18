8ce16abf5f12533f7ac919b8f113b6d33ec32701
Abdullah Altamimi <abdullah.webdeveloper@gmail.com>
2016-10-18 19:45:58 +0400

    Add POST for todos

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// Mock data, as we are still not using a real database.
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to Market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Go to Work',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -43,6 +36,26 @@ app.get('/todos/:id', function(req, res){
 	}
 });
 
+// POST.  / todos
+
+app.post('/todos', function(req,res) {
+	var body = req.body;
+
+	// add id filed
+	body.id = todoNextId++;
+
+	// push body into array
+	todos.push(body);
+
+	res.json(body);
+})
+
 app.listen(PORT, function() {
 	console.log('The Server is Running at port ' + PORT);
 });
+
+
+
+
+
+
