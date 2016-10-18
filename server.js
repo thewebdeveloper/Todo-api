3be60f4c1ecb25b6e2a0f938b8bd103e4367ab8a
Abdullah Altamimi <abdullah.webdeveloper@gmail.com>
2016-10-18 20:51:35 +0400

    Refactor API with underscorejs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -7,27 +8,27 @@ var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
 
+// Middleware
 app.use(bodyParser.json());
 
 
+// GET /
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
+
+// GET /todos
 app.get('/todos', function(req, res){
 	res.json(todos);
 });
 
+// GET /todos/:id
 app.get('/todos/:id', function(req, res){
 
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	todos.forEach( function(todo){
-		if (todoId === todo.id) {
-			matchedTodo = todo;	
-		} 
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	
 
 	if(matchedTodo) {
 		res.json(matchedTodo);
@@ -36,20 +37,27 @@ app.get('/todos/:id', function(req, res){
 	}
 });
 
-// POST.  / todos
-
+// POST /todos
 app.post('/todos', function(req,res) {
-	var body = req.body;
+	// a validation to keep only 'description' and 'completed' from the entire object, if any.
+	var body = _.pick(req.body, 'description', 'completed');
 
-	// add id filed
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	// removing the spaces
+	body.description = body.description.trim();
+
 	body.id = todoNextId++;
 
-	// push body into array
 	todos.push(body);
 
 	res.json(body);
 })
 
+
+// Setting the Port to listen
 app.listen(PORT, function() {
 	console.log('The Server is Running at port ' + PORT);
 });
