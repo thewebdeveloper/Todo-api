083f65167fcf750e45670538743b3f32d65b1601
Abdullah Altamimi <abdullah@Abdullahs-MacBook-Pro.local>
2016-10-28 15:14:37 +0400

    Add filter and search feature

--- server.js
+++ server.js
@@ -18,9 +18,24 @@ app.get('/', function (req, res) {
 });
 
 
-// GET /todos
+// GET /todos ,, adding filter and search feature
 app.get('/todos', function(req, res){
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function(todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
+	}
+
+	res.json(filteredTodos);
 });
 
 // GET /todos/:id
